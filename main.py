5a1a64cc04e7b20c1b8764ce44a59edcf0880e87
sabato <niccolo.s04@gmail.com>
2026-08-19 16:43:10 +0200

    Aggiunto keep alive

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot online!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
 import logging
 
 from telegram.ext import ApplicationBuilder
